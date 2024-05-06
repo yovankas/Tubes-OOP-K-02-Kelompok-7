f6d6904a103d7c297b4a79d1bc303bafdc2d7163
natanael-ss <naelsteven2304@gmail.com>
2024-05-06 21:03:08 +0700

    add Plant

---
+++ src/src/Plant.java
@@ -0,0 +1,26 @@
+package src;
+
+public abstract class Plant extends Creatures {
+    private int range ;
+    private int cooldown ;
+    private int cost ;
+
+    public Plant(String name, int health, int attack_damage, int attack_speed, int cost, int range, int cooldown){
+        super(name, health, attack_damage, attack_speed);
+        this.cost = cost;
+        this.range = range ;
+        this.cooldown = cooldown ;
+    }
+
+    public int getCost() {
+        return cost ;
+    }
+
+    public int getRange() {
+        return range ;
+    }
+
+    public int getCooldown() {
+        return cooldown ;
+    }
+}
---
+++ src/src/Plants/Peashooter.java
@@ -0,0 +1,10 @@
+package src.Plants;
+
+import src.Plant;
+
+public class Peashooter extends Plant {
+    
+    public Peashooter(String name, int health, int attack_damage, int attack_speed, int cost, int range, int cooldown) {
+        super(name, health, attack_damage, attack_speed, cost, range, cooldown);
+    }
+}
