1984e9b22375f2dddfcb4f298b4b523c93d4de03
jstnlwrn <justinlawrance18@gmail.com>
2024-05-24 00:50:45 +0700

    Update WrongCommandException.java

--- src/src/Exception/WrongCommandException.java
+++ src/src/Exception/WrongCommandException.java
@@ -2,8 +2,6 @@ package src.Exception;
 
 public class WrongCommandException extends Exception{
     public WrongCommandException() {
-        super("""
-              Perintah salah! Masukkan kembali perintah kembali!
-              Gunakan "Help" untuk melihat command yang tersedia""");
+        super("Perintah salah! Masukkan kembali perintah kembali!" + "\n" + "Gunakan \"Help\" untuk melihat command yang tersedia");
     }
 }
